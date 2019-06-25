88e614f0ed43407dbe80d2cf354651990c872a90
David Moya <davidmgarcia6@gmail.com>
2019-06-25 20:03:26 +0100

    updated gradle MPAndroidChart

--- app/src/main/java/com/example/davidmgarcia6/animalschool/StatsView.java
+++ app/src/main/java/com/example/davidmgarcia6/animalschool/StatsView.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StatsView extends AppCompatActivity {
 
@@ -20,30 +22,36 @@ public class StatsView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_view);
 
-        pieChart = (PieChart) findViewById(R.id.pieChart1);
+        pieChart = findViewById(R.id.pieChart1);
         assert pieChart != null;
-        pieChart.setDescription("Your Child's Score");
+        Description description = new Description();
+        description.setText("Your Child's Score");
+        pieChart.setDescription(description);
         pieChart.setDragDecelerationFrictionCoef(0.95f);
 
-        ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(UserScore.getQuizScore(),0));// User's Quiz Score
-        entries.add(new Entry(4,1));// Number of Levels in the App
-        entries.add(new Entry(UserScore.getNumGuesses(),2));// Number of incorrect guesses
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(UserScore.getQuizScore(),"# Correct Answers"));// User's Quiz Score
+        entries.add(new PieEntry(4,"# of Levels"));// Number of Levels in the App
+        entries.add(new PieEntry(UserScore.getNumGuesses(),"# Incorrect Guesses"));// Number of incorrect guesses
 
-        ArrayList<String> labels = new ArrayList<String>();
+        List<String> labels = new ArrayList<String>();
         labels.add(0,"# Correct Answers");
         labels.add(1,"# of Levels");
         labels.add(2,"# Incorrect Guesses");
 
+
+
+
         PieDataSet dataSet = new PieDataSet(entries,"Entries");
         dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-        PieData data = new PieData(labels, dataSet); // initialize PieData
+        PieData data = new PieData(dataSet); // initialize PieData
         pieChart.setData(data);
         pieChart.invalidate();
         pieChart.animateY(2000);
 
-        pieChart.setDescription("User's Application Statistics");
+        description.setText("User's Application Statistics");
+        pieChart.setDescription(description);
 
 
     }
